a2e797d85ead845ed1fbee1080e0f233e866de1e
AkioMabuchi <mabuchiakio@gmail.com>
2020-11-01 09:19:20 +0900

    bug fixed

--- db/migrate/20200921014830_changesignupconfirmation_2.rb
+++ db/migrate/20200921014830_changesignupconfirmation_2.rb
@@ -1,6 +1,5 @@
 class Changesignupconfirmation2 < ActiveRecord::Migration[6.0]
   def change
-    add_column :signup_confirmations, :encrypted_password, :string
-    add_column :signup_confirmations, :encrypted_password_iv, :string
+
   end
 end
